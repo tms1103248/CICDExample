35f14167a44918232e143099e5ce0a3aefa05be4
Шишков <tms1103248@gmail.com>
2023-03-06 20:45:20 +0300

    add tests for controller

---
+++ src/test/java/com/example/cicdexample/controller/TestControllerTest.java
@@ -0,0 +1,27 @@
+package com.example.cicdexample.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest(TestController.class)
+class TestControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void getHello() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/test"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("hello world!"));
+    }
+}
